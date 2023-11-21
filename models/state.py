16902efb45877a4ed26035dbf4d551a183753f81
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-21 23:49:55 +0000

    test

--- models/state.py
+++ models/state.py
@@ -1,56 +1,21 @@
 #!/usr/bin/python3
-
 """ City Module for HBNB project """
-
 from models.base_model import BaseModel, Base
-
 from sqlalchemy import Column, String, ForeignKey
-
 from sqlalchemy.orm import relationship
-
 from os import getenv
-
-
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-
-
-
-class State(BaseModel, Base):
-
+class City(BaseModel, Base):
     """The city class, contains state ID and name"""
 
-
-
-    __tablename__ = "State"
-
+    __tablename__ = "cities"
     if storage_type == "db":
-
         name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade="all,delete", backref="state")
-
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref="cities")
     else:
-
         name = ""
-         @property
-
-        def cities(self):
-
-            """getter docuemnt"""
-
-            from models import storage
-
-            citiesList = []
-
-            citiesAll = storage.all(City)
-
-            for city in citiesAll.values():
-
-                if city.state_id == self.id:
-
-                    citiesList.append(city)
-
-            return citiesList
-
+        state_id = ""
